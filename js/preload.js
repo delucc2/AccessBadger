43ce762deb7ae8e1a6a3ba0226c13097f0212f90
Jared Okun <okunj20@gmail.com>
2018-10-08 02:21:12 -0400

    added make buttons method

--- js/preload.js
+++ js/preload.js
@@ -7,10 +7,11 @@ preloadState.prototype.preload = function(){
 	game.load.image("platform", "assets/platform.png");
 	game.load.image("star", "assets/star.png");
 	game.load.spritesheet("murph", "assets/character.png", 32, 48);
+	game.load.spritesheet("button_sheet", "assets/button_sheet.png", 64, 64);
 };
 
 preloadState.prototype.create = function(){
-	game.state.start("Gameplay");
+	game.state.start("Buttons");
 };
 
 preloadState.prototype.update = function(){
