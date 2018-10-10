7595b436b30ff1b50396341f3132bf63f493fa00
Corey D <coreyd2108@gmail.com>
2018-10-10 16:38:10 -0400

    Delete duplicate code

--- js/gameplay.js
+++ js/gameplay.js
@@ -144,30 +144,6 @@ gameplayState.prototype.setSelectionBlueGate = function(){
 	this.type = "blue";
 };
 
-gameplayState.prototype.turn = function(badger, wall) {
-	if (badger.body.touching.down) {
-		badger.body.velocity.y = 0;
-		badger.body.velocity.x = 75;
-	} else if (badger.body.touching.right) {
-		badger.body.velocity.y = -75;
-		badger.body.velocity.x = 0;
-	} else if (badger.body.touching.up) {
-		badger.body.velocity.y = 0;
-	  badger.body.velocity.x = -75;
-	} else if (badger.body.touching.left) {
-		badger.body.velocity.y = 75;
-		badger.body.velocity.x = 0;
-	}
-};
-
-gameplayState.prototype.access = function(badger, gate) {
-  if (badger.type !== gate.type) {
-    return true;
-  } else {
-		return false;
-	}
-};
-
 gameplayState.prototype.allowSwitch = function(wall) {
 	this.overWall = true;
 };
