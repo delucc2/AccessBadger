ad1d608fd940b0fe9ed7667628f03eb00ead140d
Corey D <coreyd2108@gmail.com>
2018-10-11 20:04:13 -0400

    Ungoof stuff

--- js/gameplay.js
+++ js/gameplay.js
@@ -151,3 +151,27 @@ gameplayState.prototype.allowSwitch = function(wall) {
 gameplayState.prototype.disallowSwitch = function(wall) {
 	this.overWall = false;
 };
+
+gameplayState.prototype.turn = function(badger, wall) {
+	if (badger.body.touching.down) {
+		badger.body.velocity.y = 0;
+		badger.body.velocity.x = 75;
+	} else if (badger.body.touching.right) {
+		badger.body.velocity.y = -75;
+		badger.body.velocity.x = 0;
+	} else if (badger.body.touching.up) {
+		badger.body.velocity.y = 0;
+	  badger.body.velocity.x = -75;
+	} else if (badger.body.touching.left) {
+		badger.body.velocity.y = 75;
+		badger.body.velocity.x = 0;
+	}
+};
+
+gameplayState.prototype.access = function(badger, gate) {
+  if (badger.type !== gate.type) {
+    return true;
+  } else {
+		return false;
+	}
+}
